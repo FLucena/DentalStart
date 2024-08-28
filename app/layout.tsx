10b7d25599ebf5898772cd892f9f84048bd1f24f
Francisco Lucena <franciscolucena90@gmail.com>
2024-08-28 05:03:06 -0300

    React v13

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Head from "next/head"; // Import Head component
 import "./globals.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -11,6 +12,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dental Start",
   description: "Tu sonrisa es el mejor accesorio.",
+  icons: {
+    icon: ['/icon.ico?v=4'],
+    apple: ['/icon.ico?v=4'],
+    shortcut: ['/icon.ico.png'],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
@@ -20,6 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content="Tu sonrisa es el mejor accesorio." />
+        <title>Dental Start</title>
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
