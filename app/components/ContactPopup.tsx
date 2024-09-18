c79cbbe53034568a658af8a3c7e0f7761c133c19
Francisco Lucena <franciscolucena90@gmail.com>
2024-09-18 16:01:24 -0300

    arreglé el form v1

--- app/components/ContactPopup.tsx
+++ app/components/ContactPopup.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 interface ContactPopupProps {
-  onClose: () => void; // Función que se llama para cerrar el popup
+  onClose: () => void;
 }
 
 const ContactPopup: React.FC<ContactPopupProps> = ({ onClose }) => {
@@ -15,16 +15,16 @@ const ContactPopup: React.FC<ContactPopupProps> = ({ onClose }) => {
   });
   const [message, setMessage] = useState("");
   const [isOpen, setIsOpen] = useState(true);
-  const [isAnimating, setIsAnimating] = useState(true); // Para controlar la animación de desvanecimiento
+  const [isAnimating, setIsAnimating] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
-  // Referencia al contenedor del popup
   const popupRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setIsOpen(true);
-    const timer = setTimeout(() => setIsAnimating(false), 300); // Tiempo de animación
-    
-    // Función para manejar clics fuera del popup
+    const timer = setTimeout(() => setIsAnimating(false), 300);
+
     const handleClickOutside = (event: MouseEvent) => {
       if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
         onClose();
@@ -32,7 +32,7 @@ const ContactPopup: React.FC<ContactPopupProps> = ({ onClose }) => {
     };
 
     document.addEventListener('mousedown', handleClickOutside);
-    
+
     return () => {
       clearTimeout(timer);
       document.removeEventListener('mousedown', handleClickOutside);
@@ -53,18 +53,19 @@ const ContactPopup: React.FC<ContactPopupProps> = ({ onClose }) => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-  
+    setIsLoading(true);
+    setMessage("");
+
     try {
       const response = await fetch('./api/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(form), // Send the form data to the API route
+        body: JSON.stringify(form),
       });
-  
-      const result = await response.json(); // Get the response from the API route
-      console.log(result.message);
+
+      const result = await response.json();
       if (result.message === 'Datos guardados con éxito') {
         setMessage('Gracias por tu mensaje. Te contactaremos pronto.');
         setForm({
@@ -74,15 +75,17 @@ const ContactPopup: React.FC<ContactPopupProps> = ({ onClose }) => {
           dias: [],
           horarios: [],
         });
+        setIsFormSubmitted(true); // Oculta el formulario y muestra el mensaje
       } else {
         setMessage('Hubo un error al enviar el formulario. Intenta nuevamente y asegúrate de completar todos los campos.');
       }
     } catch (error) {
       console.error('Error al enviar el formulario', error);
       setMessage('Hubo un error al enviar el formulario. Intenta nuevamente.');
+    } finally {
+      setIsLoading(false);
     }
   };
-  
 
   return (
     <>
@@ -99,78 +102,86 @@ const ContactPopup: React.FC<ContactPopupProps> = ({ onClose }) => {
               <i className="fa fa-times text-lg"></i>
             </button>
             <h2 className="text-xl font-extrabold mb-4 text-center text-gray-800">Contáctanos</h2>
-            <p className="mb-4 text-center text-gray-600">Nos encantaría escucharte! Por favor envíanos un mensaje.</p>
-            <form className="space-y-4" onSubmit={handleSubmit}>
-              <input
-                type="text"
-                className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition"
-                name="nombre"
-                value={form.nombre}
-                onChange={handleInputChange}
-                placeholder="Nombre"
-                maxLength={30}
-                required
-              />
-              <input
-                type="text"
-                className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition"
-                name="celular"
-                value={form.celular}
-                onChange={handleInputChange}
-                placeholder="Celular"
-                maxLength={15}
-                required
-              />
-              <textarea
-                name="motivo"
-                className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition"
-                value={form.motivo}
-                onChange={handleInputChange}
-                rows={3}
-                placeholder="Motivo de la consulta"
-                spellCheck="false"
-                required
-              />
-              <p className="text-sm">Días disponibles:</p>
-              <div className="flex justify-center">
-                <select
-                  name="dias"
-                  className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition overflow-y-auto"
-                  multiple
-                  value={form.dias}
-                  onChange={handleInputChange}
-                  size={6}
-                  required
-                >
-                  <option value="Lunes">Lunes</option>
-                  <option value="Martes">Martes</option>
-                  <option value="Miércoles">Miércoles</option>
-                  <option value="Jueves">Jueves</option>
-                  <option value="Viernes">Viernes</option>
-                  <option value="Sábado">Sábado</option>
-                </select>
-              </div>
 
-              <p className="text-sm">Turnos disponibles:</p>
-              <div className="flex justify-center">
-                <select
-                  name="horarios"
-                  className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition overflow-y-auto"
-                  multiple
-                  value={form.horarios}
-                  onChange={handleInputChange}
-                  size={2}
-                  required
-                >
-                  <option value="Mañana">Mañana</option>
-                  <option value="Tarde">Tarde</option>
-                </select>
+            {isLoading ? (
+              <div className="flex justify-center items-center h-32">
+                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600"></div>
               </div>
-              <button className="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition">
-                Enviar
-              </button>
-            </form>
-            {message && <p className="mt-4 text-center text-gray-600">{message}</p>}
+            ) : isFormSubmitted ? (  // Si el formulario fue enviado, muestra solo el mensaje
+              <p className="mt-4 text-center text-gray-600">{message}</p>
+            ) : (
+              <form className="space-y-4" onSubmit={handleSubmit}>
+                <p className="mb-4 text-center text-gray-600">Nos encantaría escucharte! Por favor envíanos un mensaje.</p>
+                <input
+                  type="text"
+                  className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition"
+                  name="nombre"
+                  value={form.nombre}
+                  onChange={handleInputChange}
+                  placeholder="Nombre"
+                  maxLength={30}
+                  required
+                />
+                <input
+                  type="text"
+                  className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition"
+                  name="celular"
+                  value={form.celular}
+                  onChange={handleInputChange}
+                  placeholder="Celular"
+                  maxLength={15}
+                  required
+                />
+                <textarea
+                  name="motivo"
+                  className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition"
+                  value={form.motivo}
+                  onChange={handleInputChange}
+                  rows={3}
+                  placeholder="Motivo de la consulta"
+                  spellCheck="false"
+                  required
+                />
+                <p className="text-sm">Días disponibles:</p>
+                <div className="flex justify-center">
+                  <select
+                    name="dias"
+                    className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition overflow-y-auto"
+                    multiple
+                    value={form.dias}
+                    onChange={handleInputChange}
+                    size={6}
+                    required
+                  >
+                    <option value="Lunes">Lunes</option>
+                    <option value="Martes">Martes</option>
+                    <option value="Miércoles">Miércoles</option>
+                    <option value="Jueves">Jueves</option>
+                    <option value="Viernes">Viernes</option>
+                    <option value="Sábado">Sábado</option>
+                  </select>
+                </div>
+
+                <p className="text-sm">Turnos disponibles:</p>
+                <div className="flex justify-center">
+                  <select
+                    name="horarios"
+                    className="w-full max-w-xs px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:border-transparent outline-none transition overflow-y-auto"
+                    multiple
+                    value={form.horarios}
+                    onChange={handleInputChange}
+                    size={2}
+                    required
+                  >
+                    <option value="Mañana">Mañana</option>
+                    <option value="Tarde">Tarde</option>
+                  </select>
+                </div>
+                <button className="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition">
+                  Enviar
+                </button>
+              </form>
+            )}
           </div>
         </div>
       )}
