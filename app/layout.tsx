a1b1821b4d454ee5b22573c17bbd6283eb3ebaba
Francisco Lucena <franciscolucena90@gmail.com>
2024-09-17 21:38:44 -0300

    google ads v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,19 @@ export default function RootLayout({
       <Head>
         <meta name="description" content="Tu sonrisa es el mejor accesorio." />
         <title>Dental Start</title>
+        
+        {/* Google Ads Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11396130788"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11396130788');
+            `,
+          }}
+        ></script>
       </Head>
       <body className={inter.className}>
         <Header />
